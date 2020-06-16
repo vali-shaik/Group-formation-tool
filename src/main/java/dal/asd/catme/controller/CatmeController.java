3fcf47daded2b02ec50e02feb6cfc87ef481d24c
Vali Shaik <vl216084@dal.ca>
2020-06-16 16:41:27 -0300

    Fetching env variables

--- src/main/java/dal/asd/catme/controller/CatmeController.java
+++ src/main/java/dal/asd/catme/controller/CatmeController.java
@@ -8,7 +8,7 @@ import javax.mail.MessagingException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -168,12 +168,14 @@ public class CatmeController {
 		return page;
 	}
 
+	
 	//Displaying Login page for authentication
 	@RequestMapping("login")
 	public String loginPage()
 	{
 		log.info("Redirected to Login page");
 		System.out.println("####System Environment Vars: "+System.getenv());
+		System.out.println("####PASSWORD ENC: "+System.getenv().get("PASSWORD_ENC"));
 		return CatmeUtil.LOGIN_PAGE;
 	}
 
